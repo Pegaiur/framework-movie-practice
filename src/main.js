2777b55d381a66f50e8e0e1255a428da3f0d18b3
Pegasus <pegasus@yunio.com>
2017-09-12 16:50:57 -0700

    add package.json

---
+++ src/main.js
@@ -0,0 +1,12 @@
+require("./style.css");
+document.write(require("./content.js"));
+
+var movies = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'},
+  ];
+
+  
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-    entry: "./entry.js",
+    entry: "./src/main.js",
     output: {
         path: __dirname,
         filename: "bundle.js"
     },
     module: {
         loaders: [
-          { test: /\.css$/, loader: "style-loader!css-loader" }
+          { test: /\.css$/, loader: "style-loader!css-loader" },
+          {
+            test: /\.js$, .jsx$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: { presets: [ 'es2015', 'react' ] }
+          }
         ]
     }
 };
