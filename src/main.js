5c752e0e03325e9f186ac2a21f504e70f0104f48
Pegasus <pegasus@yunio.com>
2017-09-14 12:21:15 -0700

    finish level 1

--- src/main.js
+++ src/main.js
@@ -9,22 +9,49 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: props.moives
+      movies: props.moives,
+      displayMovies: props.moives
     };
   }
 
+  onSearch(text) {
+    let filteredMovies = _.filter(this.state.movies, (movie) => {
+      return _.includes(movie.title, text);
+    });
+    this.setState({ displayMovies: filteredMovies });
+  }
+
   render() {
-    let moiveLis = _.map(this.props.moives, (moive, index) => {
+    let moiveLis = _.map(this.state.displayMovies, (moive, index) => {
       return <li key={index}>{moive.title}</li>;
     })
+    moiveLis = moiveLis.length > 0 ? moiveLis : <p>No Search Result :(</p>
 
     return (
       <div>
         <h1>Moive List</h1>
+        <Search clickFunc={(text) => this.onSearch(text)}/>
         <ul>{moiveLis}</ul>
       </div>
     );
   }
+}
+
+class Search extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <input id="search-text" type="text" placeholder="Search..."/>
+        <button onClick={() => this.props.clickFunc(document.getElementById('search-text').value)}>Go!</button>
+      </div>
+    );
+  }
+
 }
 
 ReactDOM.render(<App moives={movies}/>, document.getElementById('app'));
